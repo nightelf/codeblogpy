c358566867c86360028a6abdf8f51373f7145c47
Jared Kuemmerle <jared.kuemmerel@yahoo.com>
2013-04-26 23:21:08 -0700

    Added Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=255)
+    body_html = models.TextField()
+    body = models.TextField()
+    author = models.ForeignKey(Author)
+    created = DateTimeField(auto_now_add=True)
+    modified = DateTimeField(auto_now=True)
+    published = DateTimeField()
+        
+class Author(models.Model):
+    first_name = models.Charfield(max_length=30)
+    last_name = models.Charfield(max_length=40)
+    picture_file = models.Charfield(max_length=40)
+    created = DateTimeField(auto_now_add=True)
