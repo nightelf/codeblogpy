e20814e2cf9a91d4ba094d9c01eaddce6f30ee3d
Jared Kuemmerle <jared.kuemmerel@yahoo.com>
2013-04-27 11:14:47 -0700

    dev models

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class Entry(models.Model):
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    is_editor = models.BooleanField()
+    picture_file = models.CharField(max_length=40)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Article(models.Model):
     title = models.CharField(max_length=255)
     body_html = models.TextField()
     body = models.TextField()
-    author = models.ForeignKey(Author)
-    created = DateTimeField(auto_now_add=True)
-    modified = DateTimeField(auto_now=True)
-    published = DateTimeField()
-        
-class Author(models.Model):
-    first_name = models.Charfield(max_length=30)
-    last_name = models.Charfield(max_length=40)
-    picture_file = models.Charfield(max_length=40)
-    created = DateTimeField(auto_now_add=True)
+    authors = models.ManyToManyField(Author)
+    editor = models.ForeignKey(Author, related_name='the_editor')
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField()
