c8505e40cf7bd7b66d6dbb57cb67c6eaf3a38c93
Jared Kuemmerle <jared.kuemmerle@yahoo.com>
2013-07-22 07:19:43 -0700

    filters for article list

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from blog.models import Article
 from django.shortcuts import get_object_or_404
+import datetime
 
 def index(request):
-    latest_article_list = Article.objects.order_by('-published')[:5]
+    latest_article_list = Article.objects.filter(published__lte=datetime.date.today()
+        ).exclude(published=None).order_by('-published')[:10]
     template = loader.get_template('blog/index.html')
     context = RequestContext(request, {
         'latest_article_list': latest_article_list,
@@ -13,7 +15,9 @@ def index(request):
     return HttpResponse(template.render(context))
 
 def articles(request, article_title):
-    my_article = get_object_or_404(Article, slug=article_title)
+    my_article = get_object_or_404(
+        Article, slug=article_title, published__lte=datetime.date.today(), published__isnull=False
+    )
     authors = my_article.authors.all()
     template = loader.get_template('blog/article.html')
     context = RequestContext(request, {
